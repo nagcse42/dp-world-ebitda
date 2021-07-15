e6a40e0f954823c6224551104cec0fa5252a7a11
nagcse42 <nagcse42@gmail.com>
2021-07-15 22:11:02 +0400

    removing unused  objects.

--- src/app/high-chart-ebitda/high-chart-ebitda.component.ts
+++ src/app/high-chart-ebitda/high-chart-ebitda.component.ts
@@ -32,84 +32,11 @@ export class HighChartEbitdaComponent implements OnInit {
   MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   SHORT_MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-  chartData: any = {
-    chart: {
-      zoomType: 'xy'
-    },
-    title: {
-      text: 'Average Monthly Temperature and Rainfall in Tokyo'
-    },
-    subtitle: {
-      text: 'Source: WorldClimate.com'
-    },
-    xAxis: [{
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      crosshair: true
-    }],
-    yAxis: [{ // Primary yAxis
-      labels: {
-        format: '{value}°C',
-        style: {
-          color: Highcharts.getOptions().colors[1]
-        }
-      },
-      title: {
-        text: 'Temperature',
-        style: {
-          color: Highcharts.getOptions().colors[1]
-        }
-      }
-    }, { // Secondary yAxis
-      title: {
-        text: 'Rainfall',
-        style: {
-          color: Highcharts.getOptions().colors[0]
-        }
-      },
-      labels: {
-        format: '{value} mm',
-        style: {
-          color: Highcharts.getOptions().colors[0]
-        }
-      },
-      opposite: true
-    }],
-    tooltip: {
-      shared: true
-    },
-    legend: {
-      layout: 'vertical',
-      align: 'left',
-      x: 120,
-      verticalAlign: 'top',
-      y: 100,
-      floating: true,
-      backgroundColor:
-        Highcharts.defaultOptions.legend.backgroundColor || // theme
-        'rgba(255,255,255,0.25)'
-    },
-    series: [{
-      name: 'Rainfall',
-      type: 'column',
-      yAxis: 1,
-      data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
-      tooltip: {
-        valueSuffix: ' mm'
-      }
-
-    }, {
-      name: 'Temperature',
-      type: 'spline',
-      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
-      tooltip: {
-        valueSuffix: '°C'
-      }
-    }]
-  };
 
   mutliChartData: any = {
-    chart: {},
+    chart: {
+      alignTicks: false
+    },
     title: {
       text: 'EBITDA Consolidated view'
     },
@@ -141,7 +68,8 @@ export class HighChartEbitdaComponent implements OnInit {
           style: {
             color: 'black'
           }
-        }
+        },
+        endOnTick: false,
       },
       { // Secondary yAxis
         title: {
@@ -156,7 +84,9 @@ export class HighChartEbitdaComponent implements OnInit {
           }
         },
         opposite: true,
-        //min: 6000000
+        startOnTick: false,
+        //linkedTo: 0,
+        //min: 60000000
       }],
     tooltip: {
       formatter: function () {
@@ -245,7 +175,6 @@ export class HighChartEbitdaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    //Highcharts.chart('container', this.chartData);
     this.fromPeriods = this.MONTHS;
     this.toPeriods = this.MONTHS;
 
